46aa47db4a682471cb44c9e9fafb600781c49947
Bhavy Patel <bhavy0606@gmail.com>
2025-07-25 13:18:30 -0700

    feat: add Contact and Cursor components, update App structure to include new components and enhance user interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -8,6 +10,7 @@ import Services from "./components/services/Services";
 function App() {
   return (
     <>
+      <Cursor />
       <div>
         <section id="Home">
           <Navbar />
@@ -22,8 +25,11 @@ function App() {
         <section>
           <Parallax type={"portfolio"} />
         </section>
+        {/* <section> */}
+        <Portfolio />
+        {/* </section> */}
         <section>
-          <Portfolio />
+          <Contact />
         </section>
       </div>
     </>
